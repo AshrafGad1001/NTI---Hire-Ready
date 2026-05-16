e17cbd48efa3d7de1e0c308f03705d110e42dcc4
AshrafGad1001 <ashrafgad542@gmail.com>
2026-05-16 11:41:58 +0300

    Counter

---
+++ Technical/Day 12/React-Counter/src/Counter/Counter.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect, use } from "react";
+
+export default function Counter() {
+
+    const [count, setCount] = useState(0);
+
+    // useffect(() => {
+
+    //     console.log("Component Div");
+
+    // }, []);  
+
+    useEffect(() => {
+        console.log(`Count : ${count}`);
+
+    }, [count]);
+
+    return (
+        <div className="d-flex flex-column align-items-center justify-content-center vh-100">
+
+            <h1 className="display-1 fw-bold text-primary mb-4">
+                {count}
+            </h1>
+
+            <div className="d-flex gap-3">
+
+                <button
+                    className="btn btn-danger btn-lg px-4"
+                    onClick={() => setCount(prev => prev - 1)}
+                >
+                    minus
+                </button>
+
+                <button
+                    className="btn btn-secondary btn-lg px-4"
+                    onClick={() => setCount(0)}
+                >
+                    Reset
+                </button>
+
+                <button
+                    className="btn btn-success btn-lg px-4"
+                    onClick={() => setCount(prev => prev + 1)}
+                >
+                    Add
+                </button>
+
+            </div>
+
+        </div>
+    );
+}
