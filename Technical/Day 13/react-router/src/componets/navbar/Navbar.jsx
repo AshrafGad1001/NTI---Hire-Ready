ed305bef6ce2cea41c3f1e508725871200f1d5c2
AshrafGad1001 <ashrafgad542@gmail.com>
2026-05-18 01:26:08 +0300

    1st-Routing

--- Technical/Day 13/react-router/src/componets/navbar/Navbar.jsx
+++ Technical/Day 13/react-router/src/componets/navbar/Navbar.jsx
@@ -12,7 +12,6 @@ export function Navbar() {
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="about">About</Link></li>
                     <li><Link to="gallery">Gallery</Link></li>
-                    {/* <li><Link to="contact">Contact</Link></li> */}
                 </ul>
 
                 <button className="btn">
