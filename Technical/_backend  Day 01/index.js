bf6682b854edfc3e298acb76154a3cbc8ffc08d8
AshrafGad1001 <ashrafgad542@gmail.com>
2026-06-03 11:16:27 +0300

    CRUD USING ARRAY

--- Technical/_backend  Day 01/index.js
+++ Technical/_backend  Day 01/index.js
@@ -1,5 +1,31 @@
 
-function printName() {
-    console.log("Ashraf Alaa Gad");
-}
-printName();
+// function printName() {
+//     console.log("Ashraf Alaa Gad");
+// }
+// printName();
+
+
+
+
+
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+
+    if (req.url === '/' && req.method === 'GET') {
+        console.log("Home Page");
+    }
+    else if (req.url === '/about' && req.method === 'GET') {
+        console.log("About Page");
+    }
+    else if (req.url === '/contact' && req.method === 'GET') {
+        console.log("Contact Page");
+    }
+    else {
+        console.log("Page Not Found");
+    }
+
+}).listen(3000, () => {
+    console.log("Server is Running ");
+});
